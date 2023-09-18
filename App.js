8662acdb545047e58230383e50d6b38603023ed8
QuynhTranDev <quynhtd.contact@gmail.com>
2023-09-18 21:17:52 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, TextInput, View, Text } from "react-native";
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+  const [email, setEmail] = useState('')
+  const [pass, setPass] = useState('')
+  const sumbmit = () => {
+    if(email.length <= 0){
+      alert('Email ?')
+      return;
+    }
+    else if(pass.length<= 0){
+      alert('Pass ?')
+      return;
+    }
+    else {
+      alert('email: ' + email + '\npassword: ' + pass);
+      setEmail('');
+      setPass('');
+    }
+  }
+
+  return(
+    <SafeAreaView style={styles.container}>
+      <Text style={{fontSize: 30, fontWeight: "bold"}}>
+        Sign in
+      </Text>
+          <TextInput style={styles.middle} placeholder="Enter Email" value={email} onChangeText={text=>setEmail(text)}>
+
+          </TextInput>
+          <TextInput style={styles.middle} placeholder="Enter Password" value={pass} onChangeText={text=>setPass(text)}>
+
+          </TextInput>
+          <Button title="Submit" onPress={sumbmit}></Button>      
+    </SafeAreaView>
+    
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100,
   },
-});
+  middle: {
+    borderWidth: 1,
+    marginVertical: 10,
+    paddingHorizontal: 12,
+    marginTop: 20,
+  },
+}
+);
